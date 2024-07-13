265fb90d8c1de38cd86d9c59fd4efc6ef1ce112c
Vaibhav Shivaji Bhosale <textmevaibhav@gmail.com>
2024-07-13 13:32:54 +0530

    frontend completed

--- frontend/src/components/Messages/MessageContainer.jsx
+++ frontend/src/components/Messages/MessageContainer.jsx
@@ -2,25 +2,29 @@ import React from 'react'
 import Messages from './Messages'
 import MessageInput from './MessageInput'
 import { TiMessages } from "react-icons/ti";
+
 const MessageContainer = () => {
-    const NoChatSelected=true;
-  return (
-   
-    <div className='md:min-w-[450px] flex flex-col'>
-			{NoChatSelected? (
-				<NoChatSelected />
-			) : (
+    const NoChatSelecte=true;
+	
+
+
+	return (
+		<div className='md:min-w-[450px] flex flex-col'>
+			{NoChatSelecte ? <NoChatSelected/> : (
 				<>
-					<div className='bg-slate-500 px-4 py-2 mb-2'>
-						<span className='label-text'>To:</span>
-						<span className='text-gray-900 font-bold'></span>
-					</div>
-					<Messages />
-					<MessageInput />
-				</>
+				{/* Header */}
+				<div className='bg-slate-500 px-4 py-2 mb-2'>
+					<span className='label-text'>To:</span>
+					<span className='text-gray-900 font-bold'></span>
+				</div>
+				<Messages />
+				<MessageInput />
+			</>
 			)}
+				
+			
 		</div>
-  )
+	);
 }
 
 export default MessageContainer
--- frontend/src/components/Sidebar/Conversations.jsx
+++ frontend/src/components/Sidebar/Conversations.jsx
@@ -5,10 +5,7 @@ const Conversations = () => {
     <div className='py-2 flex flex-col overflow-auto'>
         <Conversation/>
         <Conversation/>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
+        
     </div>
   )
 }
