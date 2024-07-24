4454aeab4cfaea59430810d39f6a56125b48f249
Vaibhav Shivaji Bhosale <textmevaibhav@gmail.com>
2024-07-24 19:09:56 +0530

    added signup,login and logout functionality

--- frontend/src/components/Sidebar/LogoutButton.jsx
+++ frontend/src/components/Sidebar/LogoutButton.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { BiLogOut } from "react-icons/bi";
+import uselogout from '../../hooks/uselogout';
 const LogoutButton = () => {
-  return (
-    <div className='mt-auto'>
-        <BiLogOut className='w-6 h-6 text-white cursor-pointer'/>
-    </div>
-  )
-}
+	const { loading, logout } = uselogout();
+
+	return (
+		<div className='mt-auto'>
+			{!loading ? (
+				<BiLogOut className='w-6 h-6 text-white cursor-pointer' onClick={logout} />
+			) : (
+				<span className='loading loading-spinner'></span>
+			)}
+		</div>
+	);
+};
 
 export default LogoutButton;
--- frontend/src/pages/signup/signup.jsx
+++ frontend/src/pages/signup/signup.jsx
@@ -94,7 +94,10 @@ const SignUp = () => {
 					</Link>
 
 					<div>
-						<button className='btn btn-block btn-sm mt-2 border border-slate-700' >
+						<button className='btn btn-block btn-sm mt-2 border border-slate-700'
+						disabled={loading}
+						>
+							{loading ? <span className='loading loading-spinner'></span> :"Signup"}
 						</button>
 					</div>
 				</form>
