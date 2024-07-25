c94929bcef76d718628d2cd47ef5b762ddadc466
Vaibhav Shivaji Bhosale <textmevaibhav@gmail.com>
2024-07-25 20:54:07 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,11 @@ import userRoutes from "./routes/userRoutes.js"
 import { app,server } from "./socket/socket.js";
 import path from "path"
 
-const PORT=process.env.PORT || 8000;
-const __dirname=path.resolve();
 
 dotenv.config();
-const __dirname = path.resolve();
-// PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
-const PORT = process.env.PORT || 5000;
+
+const PORT=process.env.PORT || 8000;
+const __dirname=path.resolve();
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
