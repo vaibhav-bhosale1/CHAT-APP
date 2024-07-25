0f6b1832b8547e14d8a38107266392bbcb437e53
Vaibhav Shivaji Bhosale <textmevaibhav@gmail.com>
2024-07-25 12:34:17 +0530

    messages update

--- frontend/src/components/Messages/Message.jsx
+++ frontend/src/components/Messages/Message.jsx
@@ -1,22 +1,28 @@
 import React from 'react'
 
-const Message = () => {
-  return (
-    <div className='chat chat-end'>
-        <div className='chat-image avatar'>
-            <div className='w-10 rounded-full'>
-            <img src="https://api.dicebear.com/9.x/bottts/avif" />
+import { useAuthContext } from "../../context/authcontext";
+import useConversation from '../../zustand/useConversation';
+const Message = ({message}) => {
+  const { authUser } = useAuthContext();
+	const { selectedConversation } = useConversation();
+	const fromMe = message.senderId === authUser._id;
 
-            </div>
-        </div>
-        <div></div>
-        <div className="chat-bubble text-white ng-blue-500">
-         hi
-        </div>
-        <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>12:42</div>
-      
-    </div>
-  )
-}
+	const chatClassName = fromMe ? "chat-end" : "chat-start";
+	const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+	const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+ 
 
+
+	return (
+		<div className={`chat ${chatClassName}`}>
+			<div className='chat-image avatar'>
+				<div className='w-10 rounded-full'>
+					<img alt='Tailwind CSS chat bubble component' src={profilePic} />
+				</div>
+			</div>
+			<div className={`chat-bubble text-white ${bubbleBgColor} pb-2`}>{message.message}</div>
+			<div className='chat-footer opacity-50 text-xs flex gap-1 items-center'></div>
+		</div>
+	);
+};
 export default Message
--- frontend/src/components/Messages/Messages.jsx
+++ frontend/src/components/Messages/Messages.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import Message from './Message'
 import getMessages from '../../hooks/getMessages'
+import MessageSkeleton from '../skeletons/MessageSkeleton'
 const Messages = () => {
   const {messages,loading}=getMessages();
   console.log("messages",messages)
   return (
     <div className='px-4 flex-1 overflow-auto'>
-        <Message/>
-        <Message/>
-       
-      
-    </div>
+    {!loading &&
+      messages.length > 0 &&
+      messages.map((message) => (
+        <div key={message._id} >
+          <Message message={message} />
+        </div>
+      ))}
+
+    {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+    {!loading && messages.length === 0 && (
+      <p className='text-center'>Send a message to start the conversation</p>
+    )}
+  </div>
   )
 }
 
