5e8af39c27df0eb2a1bbca018c72b9785cfcc776
Vaibhav Shivaji Bhosale <textmevaibhav@gmail.com>
2024-07-25 12:55:32 +0530

    search input updated

--- frontend/src/components/Messages/Message.jsx
+++ frontend/src/components/Messages/Message.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import { extractTime } from "../../utils/extractTime";
 import { useAuthContext } from "../../context/authcontext";
 import useConversation from '../../zustand/useConversation';
 const Message = ({message}) => {
   const { authUser } = useAuthContext();
 	const { selectedConversation } = useConversation();
 	const fromMe = message.senderId === authUser._id;
-
+	const formattedTime = extractTime(message.createdAt);
 	const chatClassName = fromMe ? "chat-end" : "chat-start";
 	const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
 	const bubbleBgColor = fromMe ? "bg-blue-500" : "";
@@ -21,7 +21,7 @@ const Message = ({message}) => {
 				</div>
 			</div>
 			<div className={`chat-bubble text-white ${bubbleBgColor} pb-2`}>{message.message}</div>
-			<div className='chat-footer opacity-50 text-xs flex gap-1 items-center'></div>
+			<div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>{formattedTime}</div>
 		</div>
 	);
 };
--- frontend/src/components/Messages/Messages.jsx
+++ frontend/src/components/Messages/Messages.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Message from './Message'
 import getMessages from '../../hooks/getMessages'
 import MessageSkeleton from '../skeletons/MessageSkeleton'
 const Messages = () => {
   const {messages,loading}=getMessages();
   console.log("messages",messages)
+  const lastMessageRef=useRef()
+
+useEffect(()=>{
+  setTimeout(()=>{
+    lastMessageRef.current?.scrollIntoView({behavior:"smooth"})
+  },100)
+  lastMessageRef.current?.scrollIntoView({behavior:"smooth"})
+},[messages])
+
+
   return (
     <div className='px-4 flex-1 overflow-auto'>
     {!loading &&
       messages.length > 0 &&
       messages.map((message) => (
-        <div key={message._id} >
+        <div key={message._id} ref={lastMessageRef}>
           <Message message={message} />
         </div>
       ))}
