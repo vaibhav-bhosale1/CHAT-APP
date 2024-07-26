dda00121aaddd2992b31a742b25963b649042736
Vaibhav Shivaji Bhosale <textmevaibhav@gmail.com>
2024-07-27 00:59:23 +0530

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,8 +13,8 @@ function App() {
     <div className="p-4 h-screen flex items-center justify-center">
      <Routes>
       <Route path='/'element={authUser ? <Home /> : <Navigate to={"/login"} />}/>
-      <Route path='/login'element={authUser ? <Navigate to='/' /> : <Login />}/>
-      <Route path='/signup' element={authUser ? <Navigate to='/' /> : <Signup/>}/>
+      <Route path='/login'element={authUser ? <Navigate to='/login' /> : <Login />}/>
+      <Route path='/signup' element={authUser ? <Navigate to='/login' /> : <Signup/>}/>
 
       
      </Routes>
