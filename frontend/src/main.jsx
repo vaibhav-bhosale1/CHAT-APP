cfffb8b5fc559eab3a153416f4968f3a43cbe5a1
Vaibhav Shivaji Bhosale <textmevaibhav@gmail.com>
2024-07-26 19:42:46 +0530

    .

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <AuthContextProvider>
       <SocketContextProvider>
-        <App />
+        <App/>
       </SocketContextProvider>
     </AuthContextProvider>
   </BrowserRouter>
