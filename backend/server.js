f365841abd23182755527dc797d55cad1dff22cc
Vaibhav Shivaji Bhosale <textmevaibhav@gmail.com>
2024-07-26 13:53:25 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use("/api/auth", authroutes);
 app.use("/api/messages", messageroutes);
 app.use("/api/users", userRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
 
 app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
