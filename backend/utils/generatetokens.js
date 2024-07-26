fa8e002c4ecfd51cea77755abb080da056231f6c
Vaibhav Shivaji Bhosale <textmevaibhav@gmail.com>
2024-07-27 01:55:22 +0530

    .

--- backend/utils/generatetokens.js
+++ backend/utils/generatetokens.js
@@ -7,8 +7,7 @@ const generatetokens = (userId, res) => {
 
 	res.cookie("jwt", token, {
 		maxAge: 15 * 24 * 60 * 60 * 1000, // MS
-		httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-		sameSite:"strict",
+		
 
 	
 	});
