7d3e2687b1424bf0a5528e03efcbf464a8549444
Vaibhav Shivaji Bhosale <textmevaibhav@gmail.com>
2024-07-27 18:15:26 +0530

    .

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-xi-one-14.vercel.app/", {
+			const socket = io("https://chat-app-zeta-amber.vercel.app/", {
 				query: {
 					userId: authUser._id,
 				},
